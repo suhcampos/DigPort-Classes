6b40793aa232f4aaf33e6f3515d47baa83d13d45
Campos <you@example.com>
2025-07-28 15:24:23 -0300

    aula 7 susan

---
+++ aula7-pratica/problemas-matematicos.js
@@ -0,0 +1,25 @@
+let salarioPorhora = 30
+let horasTrabalhadas = 160
+let umaString = "salarioFinal = "
+let salarioFinal = salarioPorhora * horasTrabalhadas
+
+console.log(30*160);
+console.log(umaString + 30*160);
+console.log(salarioFinal);
+
+let precoInicial = 200
+let desconto = precoInicial * 0.10
+let precoFinal = precoInicial - desconto
+let umastring = "preço final = "
+
+console.log(umastring + precoFinal);
+
+let idade = 17
+let maiordeIdade = idade >= 18
+if (idade >= 18) {console.log("Julia é maior de 18");}
+else {console.log("Julia não é maior de 18");}
+
+let quantidadedeItens = 22
+let par = quantidadedeItens % 2 === 0
+if (quantidadedeItens % 2 === 0) {console.log("é par");}
+else {console.log("é ímpar")};
