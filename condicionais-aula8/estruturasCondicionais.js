cfe9da1f379ffc0c1af52380f3142de802687dff
Campos <you@example.com>
2025-07-31 15:37:06 -0300

    aula 8 condicionais susan

---
+++ condicionais-aula8/estruturasCondicionais.js
@@ -0,0 +1,55 @@
+//let nota = 6
+
+//if (nota >= 7) {console.log("Aluna aprovada");}
+//else if (nota === 6) {console.log("Aluna em recuperação");}
+//else {console.log("Aluna reprovada");}
+
+//let num1 = 30
+//let num2 = 45
+//let num3 = 78
+
+//if (num1 > num2 && num1 > num3) {console.log("O maior número é:", num1);}
+//else if (num2 > num3) {console.log("O maior número é:", num2);}
+//else {console.log("O maior número é:", num3)}
+
+//if (num1 < num2 && num1 < num3) {console.log("O menor número é:", num1);}
+//else if (num2 < num3) {console.log("O menor número é:", num2);}
+//else {console.log("O menor número é:", num3)}
+
+//let moeda = 3
+
+//switch (moeda) {
+    //case 1:
+        //console.log("Você escolheu converter para dólar.");
+        //break;
+    //case 2:
+      // console.log("Você escolheu converter para euro."); 
+       //break;
+    //case 3:
+        //console.log("Você escolheu converter para libra.");
+        //break;
+    //default:
+        //console.log("Moeda inválida. Tente novamente.");
+//}
+
+//let numero = -25
+
+//if (numero > 0) {console.log("Número é positivo");}
+//else if (numero === 0) {console.log("Número é igual a 0");}
+//else {console.log("Número é negativo");}
+
+let turno = "N"
+
+switch (turno) {
+    case "M":
+        console.log("Bom dia!");
+        break;
+    case "V":
+        console.log("Boa tarde!");
+        break;
+    case "N":
+        console.log("Boa noite!");
+        break;
+    default:
+        console.log("Valor inválido");
+}
