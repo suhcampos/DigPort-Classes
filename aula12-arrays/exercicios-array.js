a3e75f6b53930a0ae089321e62f88c8ed2d857a3
Campos <you@example.com>
2025-08-14 14:47:07 -0300

    aula 12 susan

---
+++ aula12-arrays/exercicios-array.js
@@ -0,0 +1,19 @@
+//let thingsIlike = ["Rafa", "Livros", "Pizza", "Doguinhos", "Música"];
+
+//console.log(thingsIlike[3]);
+//console.log(thingsIlike[0]);
+//console.log(thingsIlike[1]);
+//console.log(thingsIlike[4]);
+//console.log(thingsIlike[2]);
+//console.log(thingsIlike[2, 4]);
+
+//let size = thingsIlike.length;
+//console.log(size);
+
+//let size = thingsIlike.length = 2;
+//console.log(size);
+
+const thingsIlike = ["Rafa", "Livros", "Pizza", "Doguinhos", "Música"];
+thingsIlike.push("Harry Styles");
+console.log(thingsIlike);
+
